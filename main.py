9e64dd9e1e593e3f81e38b0f68acbb73dae6af79
Luckyflame1 <146023358+Luckyflame1@users.noreply.github.com>
2023-09-25 22:25:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "GLHF": "it means good luck have fun "
+            }
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+   print("there is no data about this sentence in the diary")
